b912a953a1770b3c3c52a1a142feea496ca1003a
Wheelinventor <info@luohuizhe.com>
2022-07-07 17:04:46 +1000

    Update queue manager version

--- client/mq-put.sh
+++ client/mq-put.sh
@@ -2,11 +2,13 @@
 export MQCCDTURL=ccdt.json
 export MQSSLKEYR=../certs/mqclient
 
-i=0
-while [ $i -le 1000 ]
+# amqsputc APPQ QM1
+
+i=1
+while [ $i -le 10 ]
 do
-    (( i++ ))
     printf "%s\n\n" "sending message $i to queue manager" | amqsputc APPQ QM1 > /dev/null
     echo "Sending message $i to queue manager"
+    (( i++ ))
 done
 
