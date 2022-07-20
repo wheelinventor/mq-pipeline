46ed4fd8f9aa4bbb175f52270e1d048f0fc720e1
Wheel Inventor <info@luohuizhe.com>
2022-07-20 19:52:16 +1000

    deploy new QMgr and run test

--- scripts/03-testing.sh
+++ scripts/03-testing.sh
@@ -14,9 +14,9 @@ queue_name=${queue_name} \
 sh > ./config/testing-job.yaml
 
 cat ./config/testing-job.yaml
-sleep 60
+sleep 30
 
 ## apply yaml file of job to run testing stub 
 oc apply -f ./config/testing-job.yaml -n ${namespace}
-sleep 60
+sleep 30
 oc logs job/mq-test-${release_name} -n ${namespace}
